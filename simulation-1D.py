c9685460618d7e62eb5956915a9f5096c1a7e8d5
Issac Roy <issacroy05@gmail.com>
2024-06-13 23:22:24 -0700

    Added damping

--- simulation-1D.py
+++ simulation-1D.py
@@ -5,6 +5,7 @@ from matplotlib.animation import FuncAnimation
 # Constants
 length = 1
 speed = 1
+gamma = 1
 resolution = 100
 num_steps = 2000
 wave_width = 0.1
@@ -17,8 +18,8 @@ x = np.arange(0, length + dx, dx)
 f = np.zeros((len(x), num_steps))
 
 # Starting conditions
-# f[:, 0] = 0.5*np.sin(2 * np.pi * x) + 0.5*np.sin(3 * np.pi * x)
-f[:, 0] = np.exp(-(x - length/2)**2 / wave_width**2)
+f[:, 0] = 0.5*np.sin(2 * np.pi * x) + 0.5*np.sin(3 * np.pi * x)
+# f[:, 0] = np.exp(-(x - length/2)**2 / wave_width**2)
 f[1:-1,1] = f[1:-1,0] + 0.5 * speed**2 * (f[2:,0] + f[:-2,0] - 2*f[1:-1,0]) * (dt/dx)**2 
 
 fig, ax = plt.subplots()
@@ -26,8 +27,9 @@ ax.set(xlim=[0, length], ylim=[-1.2, 1.2])
 line, = ax.plot(x, f[:, 0])
 
 for i in range(2, num_steps):
-    spacial_info = f[2:, i-1] + f[:-2, i-1] - 2*f[1:-1, i-1]
-    f[1:-1, i] = 2*f[1:-1, i-1] - f[1:-1, i-2] + speed**2 * spacial_info * (dt/dx)**2
+    lagrangian = f[2:, i-1] + f[:-2, i-1] - 2*f[1:-1, i-1]
+    f[1:-1, i] = (4*f[1:-1, i-1] - f[1:-1, i-2] * (2 - gamma*dt) + speed**2 * \
+        lagrangian * (dt/dx)**2) / (2 + gamma*dt)
 
 def update(frame):
     line.set_data(x, f[:, frame])
--- simulation-2D.py
+++ simulation-2D.py
@@ -1,13 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import cm
-from matplotlib.animation import FuncAnimation
+from matplotlib.animation import FuncAnimation, PillowWriter
 from tqdm import tqdm
 
 # Constants
 length = 1
-speed = 0.1
-resolution = 100
+speed = 1
+gamma = 1
+resolution = 200
 num_steps = 1000
 wave_width = 0.05
 
@@ -20,7 +21,7 @@ X, Y = np.meshgrid(x, y)
 f = np.zeros((len(x), len(x), num_steps))
 
 # Starting conditions
-f[:, :, 0] = np.exp(-((X - length/2)**2 + (Y - length/2)**2) / wave_width**2)
+f[:, :, 0] = np.exp(-((X - length/3)**2 + (Y - length/4)**2) / wave_width**2)
 f[1:-1, 1:-1, 1] = f[1:-1, 1:-1, 0] + 0.5 * speed**2 * (dt/dx)**2 * \
     (f[2:, 1:-1, 0] + f[:-2, 1:-1, 0] - 2*f[1:-1, 1:-1, 0] + 
      f[1:-1, 2:, 0] + f[1:-1, :-2, 0] - 2*f[1:-1, 1:-1, 0])
@@ -30,8 +31,10 @@ surface = ax.plot_surface(X, Y, f[:, :, 0])
 ax.set(xlim=[0, length], ylim=[0, length], zlim=[-1, 1])
 
 for i in tqdm(range(2, num_steps)):
-    spacial_info = f[2:, 1:-1, i-1] + f[:-2, 1:-1, i-1] + f[1:-1, 2:, i-1] + f[1:-1, :-2, i-1] - 4*f[1:-1, 1:-1, i-1]
-    f[1:-1, 1:-1, i] = 2*f[1:-1, 1:-1, i-1] - f[1:-1, 1:-1, i-2] + speed**2 * (dt/dx)**2 * spacial_info
+    lagrangian = f[2:, 1:-1, i-1] + f[:-2, 1:-1, i-1] + f[1:-1, 2:, i-1] + \
+        f[1:-1, :-2, i-1] - 4*f[1:-1, 1:-1, i-1]
+    f[1:-1, 1:-1, i] = (4*f[1:-1, 1:-1, i-1] - f[1:-1, 1:-1, i-2] * (2 - gamma*dt) + \
+        2 * speed**2 * (dt/dx)**2 * lagrangian) / (2 + gamma*dt)
 
 def update(frame):
     global surface
@@ -40,4 +43,5 @@ def update(frame):
     ax.set_title(f"time: {frame*dt:.2f}")
 
 anim = FuncAnimation(fig=fig, func=update, frames=num_steps, interval=dt)
+anim.save("Wave.gif", writer=PillowWriter(fps=30))
 plt.show()
